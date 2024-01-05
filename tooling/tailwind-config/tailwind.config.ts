23464ba1e330a94dbd72e34812d8820ddc1a351b
RajdeepDs <rajdeepds626@gmail.com>
2024-01-05 19:14:26 +0530

    update project structure

---
+++ tooling/tailwind-config/tailwind.config.ts
@@ -0,0 +1,53 @@
+import type { Config } from "tailwindcss";
+const config: Omit<Config, "content"> = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        background: "#010419",
+        darkIndigo: "#181353",
+        indigo: "#4C35C1",
+        blueGray: "#3A3E57",
+        gray: "#686B82",
+        fadedDarkBlue: "#101B34",
+        fadedDarkPurple: "#161433",
+        fadedPurple: "#7349C7",
+        purple: "#9B69FF",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      backgroundImage: {
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
